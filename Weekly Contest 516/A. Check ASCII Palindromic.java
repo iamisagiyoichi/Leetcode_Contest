f531486a7af5100b754d5dc1613007d8926c27ef
Isagi Yoichi <iamisagiyoichi011@gmail.com>
2026-08-23 23:43:18 +0530

    Update A. Check ASCII Palindromic.java

--- Weekly Contest 516/A. Check ASCII Palindromic.java
+++ Weekly Contest 516/A. Check ASCII Palindromic.java
@@ -1,1 +1,26 @@
+class Solution {
+    public boolean isPalindromic(String s) {
+        String a = a(s);
+        String b = b(a);
+        return c(a,b);
+    }
 
+    static String a(String a){
+        StringBuilder b = new StringBuilder();
+        for(char c : a.toCharArray()){
+            int d = c;
+            for(int e = 7; e >= 0; e--){
+                b.append((d >> e) & 1);
+            }
+        }
+        return b.toString();
+    }
+
+    static String b(String a){
+        return new StringBuilder(a).reverse().toString();
+    }
+
+    static boolean c(String a, String b){
+        return a.equals(b);
+    }
+}
